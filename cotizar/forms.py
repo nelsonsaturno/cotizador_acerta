2367e074040d8795de3243ee3ba6d261d284498f
Nelson Saturno <nelsonsaturno@gmail.com>
2016-06-26 15:32:13 -0430

    forms

--- cotizar/forms.py
+++ cotizar/forms.py
@@ -4,7 +4,7 @@ from django import forms
 from cotizar.models import *
 
 
-class ConductorForm(forms.ModelForm):
+class ConductorVehiculoForm(forms.ModelForm):
 
     class Meta:
         model = Conductor
@@ -17,8 +17,8 @@ class ConductorForm(forms.ModelForm):
             'identificacion': 'Identificación',
             'estado_civil': 'Estado Civil',
             'correo': 'Correo',
-            'telefono1': 'Teléfono 1',
-            'telefono2': 'Teléfono 2',
+            'telefono1': 'Teléfono Celular',
+            'telefono2': 'Teléfono de Trabajo',
             'historial_transito': 'Historial de Tránsito',
         }
 
@@ -30,47 +30,12 @@ class ConductorForm(forms.ModelForm):
     #     return email
 
     def save(self, commit=True):
-        conductor = super(ConductorForm, self).save(commit=False)
+        conductor = super(ConductorVehiculoForm, self).save(commit=False)
         if commit:
             conductor.save()
             return conductor
         else:
             return conductor
-
-
-class VehiculoForm(forms.ModelForm):
-
-    class Meta:
-        model = Vehiculo
-        exclude = ['conductor']
-
-        # widgets = {
-        #     'sexo': forms.widgets.Select(choices=('Masculino', 'Femenino')),
-        #     'estado_civil': forms.widgets.Select(choices=('Soltero(a)', 'Casado(a)', 'Otro')),
-        # }
-
-        # labels = {
-        #     'nombre': 'Nombre',
-        #     'apellido': 'Apellido',
-        #     'sexo': 'Sexo',
-        #     'identificacion': 'Identificación',
-        #     'estado_civil': 'Estado Civil',
-        #     'correo': 'Correo',
-        #     'telefono1': 'Teléfono 1',
-        #     'telefono2': 'Teléfono 2',
-        #     'historial_transito': 'Historial de Tránsito',
-        # }
-
-    # def clean_email(self):
-    #     email = self.cleaned_data.get('email')
-    #     username = self.cleaned_data.get('username')
-    #     if User.objects.filter(email=email).exclude(username=username).count() != 0:
-    #         raise forms.ValidationError(u'Email addresses must be unique.')
-    #     return email
-
-    def save(self, commit=True):
-        vehiculo = super(VehiculoForm, self).save(commit=False)
-        return vehiculo
 
 
 class CotizacionUpdateForm(forms.ModelForm):
--- cotizar/models.py
+++ cotizar/models.py
@@ -4,20 +4,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Conductor(models.Model):
-
-    nombre = models.CharField(max_length=20, blank=False)
-    apellido = models.CharField(max_length=20, blank=False)
-    sexo = models.CharField(max_length=10, blank=False, choices=[('Masculino', 'Masculino'), ('Femenino', 'Femenino')])
-    identificacion = models.CharField(max_length=20, blank=False)
-    estado_civil = models.CharField(max_length=10, blank=False, choices=[('Soltero(a)', 'Soltero(a)'), ('Casado(a)', 'Casado(a)'), ('Otro', 'Otro')])
-    correo = models.EmailField(blank=False)
-    telefono1 = models.CharField(max_length=20, blank=False)
-    telefono2 = models.CharField(max_length=20, blank=False)
-    historial_transito = models.PositiveSmallIntegerField(blank=False)
-    edad = models.PositiveSmallIntegerField(blank=False, default=18)
-
-
 class Marca(models.Model):
 
     nombre = models.CharField(max_length=20, blank=False)
@@ -36,24 +22,28 @@ class Modelo(models.Model):
         return self.nombre
 
 
-class Vehiculo(models.Model):
+class ConductorVehiculo(models.Model):
 
-    conductor = models.ForeignKey(Conductor, blank=False)
+    nombre = models.CharField(max_length=20, blank=False)
+    apellido = models.CharField(max_length=20, blank=False)
+    sexo = models.CharField(max_length=10, blank=False, choices=[('Masculino', 'Masculino'), ('Femenino', 'Femenino')])
+    identificacion = models.CharField(max_length=20, blank=False)
+    estado_civil = models.CharField(max_length=10, blank=False, choices=[('Soltero(a)', 'Soltero(a)'), ('Casado(a)', 'Casado(a)'), ('Otro', 'Otro')])
+    correo = models.EmailField(blank=False)
+    telefono1 = models.CharField(max_length=20, blank=False)
+    telefono2 = models.CharField(max_length=20, blank=False, default=" ")
+    historial_transito = models.PositiveSmallIntegerField(blank=False)
+    edad = models.PositiveSmallIntegerField(blank=False, default=18)
     marca = models.ForeignKey(Marca, blank=False)
     modelo = models.ForeignKey(Modelo, blank=False)
     anio = models.PositiveSmallIntegerField(blank=False)
     valor = models.PositiveIntegerField(blank=False)
-    antiguedad = models.CharField(max_length=30, blank=False, default='Menor',
-                                  choices=[('Menor', 'Menor o igual a 3 años'),
-                                           ('Mayor', 'Mayor a 3 años')])
-    uso = models.PositiveSmallIntegerField(blank=False)
     importacion_piezas = models.BooleanField(default=False)
 
 
 class Cotizacion(models.Model):
 
-    conductor = models.ForeignKey(Conductor, blank=False)
-    vehiculo = models.ForeignKey(Vehiculo, blank=False)
+    conductor = models.ForeignKey(ConductorVehiculo, blank=False)
     corredor = models.ForeignKey(User)
     lesiones_corporales = models.CharField(max_length=30, blank=False, default='25,000.00/50,000.00',
                                            choices=[('5,000.00/10,000.00', '5,000.00/10,000.00'),
