0cc26c4d39ae3aedefdb081872f4a84dd7424057
Nelson Saturno <nelsonsaturno@gmail.com>
2016-07-13 14:56:45 -0430

    cambio en admin django

--- cotizar/admin.py
+++ cotizar/admin.py
@@ -1,7 +1,28 @@
 from django.contrib import admin
 from cotizar.models import *
 
+
+class ModeloAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'nombre',
+        'marca',
+        'descuento',
+        'recargo',
+        'created_at',
+        'updated_at'
+    )
+    search_fields = (
+        'id',
+        'nombre',
+        'marca',
+        'descuento',
+        'recargo',
+        'created_at',
+        'updated_at'
+    )
+
 admin.site.register(ConductorVehiculo)
 admin.site.register(Marca)
-admin.site.register(Modelo)
+admin.site.register(Modelo, ModeloAdmin)
 admin.site.register(Cotizacion)
