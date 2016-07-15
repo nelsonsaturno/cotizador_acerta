acfc31ab85636b57a1f3fbeb7c8c67ccfbbe3282
guillen <oguillen@turpialdev.com>
2016-07-15 15:31:02 -0400

    starting corredor detail

--- reportes/urls.py
+++ reportes/urls.py
@@ -7,4 +7,8 @@ urlpatterns = patterns(
         r'^corredores/$',
         CorredorVendedorListView.as_view(),
         name='corredor-vendedor-list'),
+    url(
+        r'^corredores/detail/(?P<id>\d+)/$',
+        CorredorVendedorDetailView.as_view(),
+        name='corredor-vendedor-detail'),
 )
--- reportes/views.py
+++ reportes/views.py
@@ -1,15 +1,32 @@
 from django.shortcuts import render_to_response
-from django.template.context import RequestContext
-from django.views.generic import ListView
+from django.views.generic import *
+from cotizador_acerta.views_mixins import LoginRequiredMixin
 from darientSessions.models import *
+from cotizar.models import *
 
 
-class CorredorVendedorListView(ListView):
+class CorredorVendedorListView(LoginRequiredMixin, ListView):
     model = DatosCorredor
     template_name = 'reportes/corredor_vendedor_list.html'
 
     def get_context_data(self, **kwargs):
-        context = super(CorredorVendedorListView, self).get_context_data(**kwargs)
+        context = super(
+            CorredorVendedorListView, self).get_context_data(**kwargs)
         vendedores = CorredorVendedor.objects.all()
         context['vendedores'] = vendedores
         return context
+
+
+class CorredorVendedorDetailView(LoginRequiredMixin, TemplateView):
+    template_name = 'reportes/corredor_detail.html'
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        user = User.objects.get(pk=kwargs['id'])
+        context['user'] = user
+        if user.groups.first().name == 'corredor':
+            context['corredor'] = DatosCorredor.objects.get(user=user)
+        cotizaciones = Cotizacion.objects.filter(corredor=user)
+        context['cotizaciones'] = cotizaciones
+        return self.render_to_response(context)
+
