395a44e1b560e3d55ddb7b826d1e504cdaf1162d
Nelson Saturno <nelsonsaturno@gmail.com>
2016-08-01 21:11:08 -0430

    arreglo correos

--- cotizador_acerta/settings.py
+++ cotizador_acerta/settings.py
@@ -144,13 +144,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'es'
+LANGUAGE_CODE = 'es-PA'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
 
