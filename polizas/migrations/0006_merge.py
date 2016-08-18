530e3ca5cac348521ed593334bb873bc0613a775
guillen <oguillen@turpialdev.com>
2016-08-18 15:41:49 -0400

    manual_corredores en email de bienvenida

---
+++ polizas/migrations/0006_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('polizas', '0005_auto_20160818_0603'),
+        ('polizas', '0001_initial'),
+    ]
+
+    operations = [
+    ]
