cd17cec7a8b4c85ca8e9e35f15fb48888f3fda97
Nelson Saturno <nelsonsaturno@gmail.com>
2016-08-24 16:19:08 -0430

    merge master y polizas

--- cotizador_acerta/settings.py
+++ cotizador_acerta/settings.py
@@ -100,18 +100,18 @@ WSGI_APPLICATION = 'cotizador_acerta.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-# DATABASES = {}
-# DATABASES['default'] = dj_database_url.config()
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dfegmb1cg7i64l',
-        'USER': 'xygjjzwmiompmc',
-        'PASSWORD': 'IuzXK7d7orNXGRc9rng4xaQn_a',
-        'HOST': 'ec2-54-225-117-56.compute-1.amazonaws.com',
-        'PORT': '5432',
-    }
-}
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config()
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'dfegmb1cg7i64l',
+#         'USER': 'xygjjzwmiompmc',
+#         'PASSWORD': 'IuzXK7d7orNXGRc9rng4xaQn_a',
+#         'HOST': 'ec2-54-225-117-56.compute-1.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
