3bdf68b281b49cee7d170dd99e06b3e81d9d5550
Paolangela Espinal <pao93esp@gmail.com>
2016-09-15 05:47:34 -0430

    Incluye HTML de PDF de pólizas, sin datos del formulario.

--- polizas/urls.py
+++ polizas/urls.py
@@ -6,4 +6,12 @@ urlpatterns = patterns(
     url(r'^solicitar/(?P<pk>\d+)/$',
         SolicitudPoliza.as_view(),
         name='solicitar'),
+
+    url(r'^prueba/$',
+        Test.as_view(),
+       name='prueba'),
+
+    url(r'^prueba/generacion$',
+        GeneracionPDF.as_view(),
+       name='generacion'),
 )
--- polizas/views.py
+++ polizas/views.py
@@ -14,6 +14,8 @@ from django.template.loader import get_template
 from django.core.mail import EmailMessage
 from django.views.defaults import page_not_found
 from django.contrib.humanize.templatetags.humanize import *
+import xhtml2pdf
+from xhtml2pdf import pisa
 import json
 
 
@@ -22,14 +24,38 @@ class SolicitudPoliza(LoginRequiredMixin, generic.CreateView):
     form_class = SolicitudClienteForm
     model = SolicitudPoliza
 
+    def form_valid(self, form):
+
+        ########################################################
+        # Metodo a ejecutarse luego de que el formulario es 
+        # completado y valido. Renderiza el PDF.
+        ########################################################
+
+        context = Context({'pagesize':'letter'}) 
+        template = get_template('polizas/prueba_pdf.html')
+        html  = template.render(context)
+
+        file = open("polizas/"+'prueba.pdf', "w+b")
+        pisaStatus = pisa.CreatePDF(html.encode('utf-8'), dest=file,
+            encoding='utf-8')
+
+        file.seek(0)
+        pdf = file.read()
+        file.close()
+   
+        return HttpResponse(pdf, 'application/pdf')
+        
+
+
     def get(self, request, *args, **kwargs):
+
         self.object = None
         context = self.get_context_data(**kwargs)
         cot = Cotizacion.objects.get(pk=kwargs['pk'])
         context['cotizacion'] = cot
         user = User.objects.get(username=cot.corredor)
         if user.groups.first() != 'super_admin' and user.groups.first() != 'admin':
-            context['corredor_pol'] = DatosCorredor.objects.get(user=user)
+            passcontext['corredor_pol'] = DatosCorredor.objects.get(user=user)
         return self.render_to_response(context)
 
     def get_context_data(self, **kwargs):
@@ -49,3 +75,32 @@ class PagoTarjeta(generic.FormView):
     form_class = PagoForm
     template_name = "polizas/pago.html"
     success_url = reverse_lazy('confirmacion_pago')
+
+
+########################################################
+# Vistas de prueba
+########################################################
+
+# class Test(generic.TemplateView):
+#     template_name = "polizas/generacion_PDF.html"
+
+# class GeneracionPDF(LoginRequiredMixin, generic.ListView):
+#     model = SolicitudPoliza
+#     template_name = 'polizas/prueba_pdf.html'
+
+#     def get_queryset(self):
+
+#         context = Context({'pagesize':'letter'}) 
+#         template = get_template('polizas/prueba_pdf.html')
+#         html  = template.render(context)
+
+#         file = open("polizas/"+'prueba.pdf', "w+b")
+#         pisaStatus = pisa.CreatePDF(html.encode('utf-8'), dest=file,
+#             encoding='utf-8')
+
+#         file.seek(0)
+#         pdf = file.read()
+#         file.close()
+   
+#         return HttpResponse(pdf, 'application/pdf')
+        
