1b4a1601292c35f1fe7eddbffd22bcd3c51019f4
Nelson Saturno <nelsonsaturno@gmail.com>
2016-09-18 23:55:22 -0430

    arreglando migraciones

--- polizas/admin.py
+++ polizas/admin.py
@@ -1,36 +1,36 @@
 from django.contrib import admin
 from polizas.models import *
 
-class SolicitudAdmin(admin.ModelAdmin):
-    list_display = (
-        'id',
-        'cotizacion',
-    )
-    search_fields = (
-        'id',
-        'cotizacion',
-    )
+# class SolicitudAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'id',
+#         'cotizacion',
+#     )
+#     search_fields = (
+#         'id',
+#         'cotizacion',
+#     )
 
-class ReferenciaAdmin(admin.ModelAdmin):
-    list_display = (
-        'id',
-        'nombre',
-    )
-    search_fields = (
-        'id',
-        'nombre',
-    )
+# class ReferenciaAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'id',
+#         'nombre',
+#     )
+#     search_fields = (
+#         'id',
+#         'nombre',
+#     )
 
-class ExtraDatosAdmin(admin.ModelAdmin):
-    list_display = (
-        'id',
-        'conductor',
-    )
-    search_fields = (
-        'id',
-        'conductor',
-    )
+# class ExtraDatosAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'id',
+#         'conductor',
+#     )
+#     search_fields = (
+#         'id',
+#         'conductor',
+#     )
 
-admin.site.register(SolicitudPoliza, SolicitudAdmin)
-admin.site.register(Referencia, ReferenciaAdmin)
-admin.site.register(ExtraDatosCliente, ExtraDatosAdmin)
+# admin.site.register(SolicitudPoliza, SolicitudAdmin)
+# admin.site.register(Referencia, ReferenciaAdmin)
+# admin.site.register(ExtraDatosCliente, ExtraDatosAdmin)
