785b653ba971a4fff21b274cb8f540455f1a0faa
guillen <oguillen@turpialdev.com>
2016-09-24 23:40:25 +0200

    some migrations from polizas

---
+++ darientSessions/migrations/0026_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('darientSessions', '0025_auto_20160923_0522'),
+        ('darientSessions', '0025_merge'),
+    ]
+
+    operations = [
+    ]
---
+++ polizas/migrations/0005_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('polizas', '0004_auto_20160923_0628'),
+        ('polizas', '0004_auto_20160924_2131'),
+    ]
+
+    operations = [
+    ]
