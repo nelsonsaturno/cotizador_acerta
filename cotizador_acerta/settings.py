51d771f830166037d4a99d5e837c050dcc094af3
guillen <oguillen@turpialdev.com>
2016-09-25 22:06:12 +0200

    reparando setting

--- cotizador_acerta/settings.py
+++ cotizador_acerta/settings.py
@@ -134,16 +134,16 @@ WSGI_APPLICATION = 'cotizador_acerta.wsgi.application'
 #       DESARROLLO!!
 ########################################
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd339j965q98db5',
-        'USER': 'rthjlipirhwtsb',
-        'PASSWORD': 'YpQrpQvkg5p1AyXJ83OqCRA2tH',
-        'HOST': 'ec2-54-243-190-37.compute-1.amazonaws.com',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'd339j965q98db5',
+#         'USER': 'rthjlipirhwtsb',
+#         'PASSWORD': 'YpQrpQvkg5p1AyXJ83OqCRA2tH',
+#         'HOST': 'ec2-54-243-190-37.compute-1.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
 
 #####################################################
 
