fa19c251f4bdc4be0f775ee44cc8cf66027dba1f
guillen <oguillen@turpialdev.com>
2016-09-30 00:27:38 +0200

    time-zone

--- cotizador_acerta/settings.py
+++ cotizador_acerta/settings.py
@@ -134,16 +134,16 @@ DATABASES['default'] = dj_database_url.config()
 #       DESARROLLO!!
 ########################################
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd339j965q98db5',
-        'USER': 'rthjlipirhwtsb',
-        'PASSWORD': 'YpQrpQvkg5p1AyXJ83OqCRA2tH',
-        'HOST': 'ec2-54-243-190-37.compute-1.amazonaws.com',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'd339j965q98db5',
+#         'USER': 'rthjlipirhwtsb',
+#         'PASSWORD': 'YpQrpQvkg5p1AyXJ83OqCRA2tH',
+#         'HOST': 'ec2-54-243-190-37.compute-1.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
 
 #####################################################
 
@@ -187,7 +187,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'es-PA'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Panama'
 
 USE_I18N = True
 
